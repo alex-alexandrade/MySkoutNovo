afcd7f193891fd051bf460866b069aaccae2f9b3
alex.sandrade <alex.sandrade5@senacsp.edu.br>
2022-08-10 09:15:31 -0300

    My Skout

---
+++ app/src/main/java/br/com/myskout/myskout/VagasFragment.java
@@ -0,0 +1,21 @@
+package br.com.myskout.myskout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class VagasFragment extends Fragment {
+    
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vagas, container, false);
+    }
+}
